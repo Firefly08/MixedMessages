4bd611603b80665b1df67847aa835d0f8951cb1e
Firefly08 <horearem@gmail.com>
2021-03-01 19:13:36 +0000

    Small changes to the files

--- myScript.js
+++ myScript.js
@@ -1,18 +1,19 @@
 const onClick = document.querySelector(".button")
 
 const jokes = [
-    "What rock group has four men that don't sing? Mount Rushmore.",
-    "When I was a kid, my mother told me I could be anyone I wanted to be. Turns out, identity theft is a crime.",
-    "A guy goes to his doctor because he can see into the future. The doctor asks him,\"How long have you suffered from that condition?\" The guy tells him, \"Since next Monday.\"",
-    "What do sprinters eat before a race? Nothing, they fast!",
-    "What concert costs just 45 cents? 50 Cent featuring Nickelback!",
-    "What do you call a mac 'n' cheese that gets all up in your face? Too close for comfort food!",
-    "Why couldn't the bicycle stand up by itself? It was two tired!",
+    "What's the best thing about Switzerland? I don't know, but their flag is a huge plus.",
+    "Where do you find a cow with no legs? Right where you left it.",
+    "Why aren't koalas actual bears? They don't meet the koalafications.",
+    "What's the difference between a hippo and a zippo? One is really heavy, and the other is a little lighter.",
+    "What do Alexander the Great and Winnie the Pooh have in common? Same middle name.",
+    "What did the mayonnaise say when the refrigerator door was opened? Close the door, I'm dressing.",
+    "I used to be addicted to soap. But I'm clean now.",
     "Did you hear about the restaurant on the moon? Great food, no atmosphere!",
     "How many apples grow on a tree? All of them!",
     "Did you hear the rumor about butter? Well, I'm not going to spread it!",
     "I like telling Dad jokes. Sometimes he laughs!",
-    "To whoever stole my copy of Microsoft Office, I will find you. You have my Word!"
+    "To whoever stole my copy of Microsoft Office, I will find you. You have my Word!",
+    "They all laughed when I said I wanted to be a comedian. Well, they're not laughing now!"
 ]
 
 function getJoke() {
